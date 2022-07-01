a58f8b90fd10203b88a66448deb14fee4ca539f9
Jens Reidel <adrian@travitia.xyz>
2022-07-01 21:29:26 +0200

    davinci: Calyx bringup

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-calyx
 
 # Power
 PRODUCT_COPY_FILES += \
