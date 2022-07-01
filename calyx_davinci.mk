d58b9bc78316bbb06321892f4d4f87c4d8c58368
Jens Reidel <adrian@travitia.xyz>
2022-07-01 21:29:26 +0200

    davinci: Calyx bringup

--- calyx_davinci.mk
+++ calyx_davinci.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit some common stuff.
+$(call inherit-product, vendor/calyx/config/common.mk) 
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -11,10 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-PRODUCT_NAME := aosp_davinci
+PRODUCT_NAME := calyx_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
